6bfdf1f83c1f4c12bb5c1e78b62f6e25d29100c7
Nicolas <nicolas.tsan@gmail.com>
2017-08-01 13:26:15 +0200

    basic login

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,12 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',   to: 'users#new'
+  resources :users
+
+#login logout system
+	get 'sessions/new'
+  
+  get '/login', to: 'sessions#new'
+ 	post '/login', to: 'sessions#create'
+ 	delete '/logout', to: 'sessions#destroy'
 end
